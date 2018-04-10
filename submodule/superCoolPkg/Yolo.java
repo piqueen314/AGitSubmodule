2a58834efc5a68918ec4c79cc4e51ae6e6483aab
Cece Hedrick <cecehedrick@gmail.com>
2018-04-09 18:32:36 -0600

    adds a print to cool method

--- submodule/superCoolPkg/Yolo.java
+++ submodule/superCoolPkg/Yolo.java
@@ -2,5 +2,6 @@ package AGitSubmodule.submodule.superCoolPkg;
 public class Yolo {
     public static void cool(String hellYeah){
         System.out.print(hellYeah);
+        System.out.print("The cool method has run!");
     }
 }
